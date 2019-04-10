e6ee8bfe22b6e2cdcb047a3aa57e7050eb8bc87d
Ciro <cirob_100@hotmail.com>
2019-04-10 10:33:36 -0300

    cosas de c

--- lib.c
+++ lib.c
@@ -3,8 +3,30 @@
 /** STRING **/
 
 char* strRange(char* a, uint32_t i, uint32_t f) {
-
-    return 0;
+	uint32_t len = 0;
+	char* null? = a;
+	while (a != null) {
+		len++;
+		a = a->next
+	}
+	if (i>f) {
+		return a;
+	} else if (i > len){
+		return null;
+	} else if (f > len) {
+		f = len;
+	} else {
+		char* res = null;
+		uint32_t j = 0;
+		while (j < i) {
+			a = a->next;
+		}
+		while (j <= f);
+		res->dato = a->dato;
+		res->next = a->next;
+		a = a->next;
+	}
+    return res;
 }
 
 /** Lista **/
